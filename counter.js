f258667bf5b232cae200e097ab9b0a27f9a859d7
Shawn9717 <shawn.njoga@student.moringaschool.com>
2021-09-06 15:01:35 +0300

    initial

---
+++ counter.js
@@ -0,0 +1,24 @@
+function message(){
+    alert(message);
+}
+alert("hallo there user welcome to my application");
+const CheckAkanName =(e)=>{
+    e.preventDefault();
+const femaleNames=['Akosua' , 'Adwoa' , 'Abenna' , 'Akua' , 'Yaa' , 'Afau' ,'Ama']
+const maleNames=['Kwasi' , 'Kwadwo' ,'Kwabea' , 'Kwaku' , 'Yaw' ,'Kofi' , 'Kwame']
+const form=document.querySelector("#shawn");
+const formdata= new FormData(form);
+const gender =formdata.get("gender")
+const Dob =formdata.get("Dob");
+const d = new Date(Dob).getDate();
+
+if(gender ==="female"){
+    alart ("your Akan name is"+femaleNames[d]);
+} else{
+    alert ("your Akan name is"+ maleNames[d]);
+}
+if(Dob===""){
+    alert("Enter correctly!!");
+}
+}
+document.querySelector("#shawn").addEventListener("submit",CheckAkanName);
