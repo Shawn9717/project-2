a0632a8d17cbf39f0c0749427e78445cb9a162af
Shawn9717 <shawn.njoga@student.moringaschool.com>
2021-09-15 09:49:23 +0300

    initial

--- counter.js
+++ counter.js
@@ -9,12 +9,12 @@ const Dob =formdata.get("Dob");
 const d = new Date(Dob).getDate();
 
 if(gender ==="female"){
-    alart ("your Akan name is"+femaleNames[d]);
+    alert ("your Akan name is"+femaleNames[d]);
 } else{
     alert ("your Akan name is"+ maleNames[d]);
 }
 if(Dob===""){
-    alert("Enter correctly!!");
+    alert("Enter correctly");
 }
 }
 document.querySelector("#shawn").addEventListener("submit",CheckAkanName);
